02b758e248a9046d9e8af97af3cbbdbba9eec0f0
huangminlinux <380108184@qq.com>
2017-08-08 14:42:53 +0800

    add ios project

---
+++ ios/RCTJCoreModule/RCTJCoreModule.h
@@ -0,0 +1,13 @@
+//
+//  RCTJCoreModule.h
+//  RCTJCoreModule
+//
+//  Created by oshumini on 2017/8/8.
+//  Copyright © 2017年 HXHG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTJCoreModule : NSObject
+
+@end
